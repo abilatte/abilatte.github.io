34231f29d5defbeb18cd1a525ee584e2f415f693
阿比拿铁 <201858090+abilatte@users.noreply.github.com>
2026-07-20 22:42:51 +0800

    更新 2026-07-20 22:42

--- 发布.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-where pwsh >nul 2>nul
-if %errorlevel%==0 (
-  pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0publish.ps1"
-) else (
-  powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0publish.ps1"
-)
